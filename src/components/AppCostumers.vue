0c687eb575c3017edeb29bff0ff8aa255661583b
Alexanderrrr <acons1987@gmail.com>
2018-11-23 18:41:41 +0100

    Layouts

---
+++ src/components/AppCostumers.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div>
+    <h1>App Costumers</h1>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <router-link class="navbar-brand" to="/costumers">Costumers</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/products">Products</router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+  nav {
+    margin-top: -60px;
+  }
+</style>
